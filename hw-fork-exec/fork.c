a776025c95ceb2561413281096b055e9b9a8e2ed
Justin Andrew McKeen <jmckeen@everglades.cs.byu.edu>
2022-09-19 13:49:22 -0600

    Fork Completed

--- hw-fork-exec/fork.c
+++ hw-fork-exec/fork.c
@@ -1,11 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
 	int pid;
+	int child_status;
+	int pipefd[2];
 
 	printf("Starting program; process has pid %d\n", getpid());
+
+	FILE * fp; fp = fopen("fork-output.txt", "w+"); 
+	fprintf(fp, "%s", "BEFORE FORK\n");
+   fflush(fp);
+	
+	
+	pipe(pipefd);
+
 
 	if ((pid = fork()) < 0) {
 		fprintf(stderr, "Could not fork()");
@@ -14,16 +25,45 @@ int main(int argc, char *argv[]) {
 
 	/* BEGIN SECTION A */
 
+	fprintf(fp, "%s", "SECTION A\n");
+	fflush(fp); 
+
 	printf("Section A;  pid %d\n", getpid());
-	sleep(30);
+	//sleep(30);
 
 	/* END SECTION A */
 	if (pid == 0) {
 		/* BEGIN SECTION B */
+		fprintf(fp, "%s", "SECTION B\n");
+      fflush(fp);
+      FILE * fd_one = fdopen(pipefd[1],"w");
+      fputs( "hello from Section B\n", fd_one);
+
+		close(pipefd[0]);
 
 		printf("Section B\n");
-		sleep(30);
+		//sleep(30);
+		//sleep(30);
 		printf("Section B done sleeping\n");
+
+		// NEW CODE
+		char *newenviron[] = { NULL };
+
+
+		printf("Program \"%s\" has pid %d. Sleeping.\n", argv[0], getpid());
+		//sleep(30);
+
+		if (argc <= 1) {
+			printf("No program to exec.  Exiting...\n");
+			exit(0);
+		}
+
+		printf("Running exec of \"%s\"\n", argv[1]);
+		int file_descriptor = fileno(fp);
+      dup2(fileno(fp), 1);
+		execve(argv[1], &argv[1], newenviron);
+		printf("End of program \"%s\".\n", argv[0]);
+		// NEW CODE END
 
 		exit(0);
 
@@ -31,9 +71,19 @@ int main(int argc, char *argv[]) {
 	} else {
 		/* BEGIN SECTION C */
 
+		fprintf(fp, "%s", "SECTION C\n");
+      fflush(fp); 
+		wait(&child_status);
+
+		close(pipefd[1]);
+      FILE * fd;
+      fd = fdopen(pipefd[0],"r");
+		char buf[100];
+      fgets(buf,100,fd);
+      fputs(buf,stdout);  
+
 		printf("Section C\n");
-		sleep(30);
-		sleep(30);
+		//sleep(30);
 		printf("Section C done sleeping\n");
 
 		exit(0);
@@ -43,7 +93,7 @@ int main(int argc, char *argv[]) {
 	/* BEGIN SECTION D */
 
 	printf("Section D\n");
-	sleep(30);
+	//sleep(30);
 
 	/* END SECTION D */
 }
